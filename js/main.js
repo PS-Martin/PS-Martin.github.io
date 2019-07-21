0cf2f0a19e3a031d1552d3554646bc45419ed176
PS-Martin <50917646+PS-Martin@users.noreply.github.com>
2019-07-21 17:07:34 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -272,7 +272,11 @@ PlayState._onHeroVsDoor = function (hero, door) {
 
 //--------------------------------------------------------------
 PlayState._handleInput = function () {
-    
+    window.addEventListener("deviceorientation", function(event) {
+    let x = event.gamma;
+     	let c = (x/(-x))
+    	this.hero.move(c);
+}, true);
 
 
     if (this.keys.left.isDown) { // move hero left
